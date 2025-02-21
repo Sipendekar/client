2182ecb5c7de8aa1c13cf63840573aa1542f1d97
RockieBoy <changesmoothyman04@gmail.com>
2025-02-21 13:22:21 +0700

    second commit

--- resources/views/content/form.blade.php
+++ resources/views/content/form.blade.php
@@ -6,8 +6,8 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-7 text-center mx-auto">
-                    <h1 class="text-white pt-3 mt-n5">Road Detection</h1>
-                    <p class="lead text-white mt-3">Sistem Pendekteksi<br/> Jalanan Berlubang </p>
+                    <h1 class="text-white pt-3 mt-n5">SI PENDEKAR</h1>
+                    <p class="lead text-white mt-3">Sistem Deteksi dan Prediksi<br/> Kerusakan Jalan Berbasis AI </p>
                 </div>
             </div>
         </div>
